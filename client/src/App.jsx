389581d5d26101210dcc5c0f36de03b7a86d67a1
*pknowledge* <mishrasangeeta944@gmail.com>
2024-06-11 12:48:18 +0530

    register api in fronted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import Register from "./Register"
-
+import axios from 'axios'
 function App() {
+
+  axios.defaults.baseURL = "https://localhost:4000";
+  axios.defaults.withCredentials = true;
   return (
   <Register/>
   )
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
-
+import axios from 'axios';
 
 const Register = () => {
     const[username,setUserName] = useState('');
     const[password,setPassword] = useState('');
+
+async function Registration(e){
+    e.preventDefault();
+    await axios.post('/register',{username,password})
+}
+
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-        <form className="w-64 mx-auto mb-12">
+        <form className="w-64 mx-auto mb-12" onSubmit={Registration}>
             <input value={username} onChange={e => setUserName(e.target.value)} type="text" placeholder="username" className="block w-full rounded-sm p-2 mb-2"/>
             <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="password" className="block w-full rounded-sm p-2 mb-2"/>
             <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
