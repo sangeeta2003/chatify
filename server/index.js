524912bc3a641853f5a49350a18197b68f4adb4f
*pknowledge* <mishrasangeeta944@gmail.com>
2024-06-11 12:15:57 +0530

    connect to mongodb done

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
 
 const app = express();
 
@@ -7,7 +14,7 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/register',(req,res)=>{
-    
+
 })
 
 app.listen(4000);
